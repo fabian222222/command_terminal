c1e29ce40385ae8dc48543c5e04c857b4a4dcde5
fabian222222 <fabian.zuo@supinternet.fr>
2022-03-11 11:11:48 +0100

    add products field and custom field | status : fixed

--- server/src/FormValidators/Command/CommandForm.ts
+++ server/src/FormValidators/Command/CommandForm.ts
@@ -3,8 +3,10 @@ import { body } from "express-validator"
 export const CommandForm = [
     body("status")
         .isAlpha().withMessage("This field need to contain only caracters"),
-    body("amount")
-        .isNumeric().withMessage("This field need to contain only numbers"),
     body("userId").optional()
-        .isNumeric().withMessage("UserID need to be of type integer")
+        .isNumeric().withMessage("UserID need to be of type integer"),
+    body("custom")
+        .isBoolean().withMessage("This field need to be a boolean"),
+    body("products")
+        .isArray().withMessage("Products need to be an array of product")
 ]
